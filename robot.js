4edff28953875e476fa3417c20d96eac93a36397
Steve Nelson <snelson@zenmanenergy.org>
2015-10-01 12:53:24 -0400

    bug fixes
    Updated the robot.js file and added a crossdomain.xml file

--- robot.js
+++ robot.js
@@ -1,41 +1,113 @@
+var connection;
 (function (ext) {
+    	console.log("robot v1");
     // Cleanup function when the extension is unloaded
+    
     ext._shutdown = function() {};
 
     ext._getStatus = function() {
         return { status:2, msg:'Ready' };
     };
-
-    ext.leftWheelOn = function(direction) {
-		console.log("leftWheelOn",direction);
-        /*
-         * $.ajax({
-            type: "GET",
-            url: cmdUrl,
-            //dataType: "jsonp", // hack for the not origin problem - replace with CORS based solution
-            success: function(data) {
-                console.log("setCircle success");
-            },
-            error: function(jqxhr, textStatus, error) { // have to change this coz jasonp parse error
-                console.log("Error setCircle: ", error);
-            }
-        }); // nb: GET is including the javascript callback. Do I need this for one-way call?
-        
-         */
+	
+	ext.connect = function(ip) {
+    	console.log("connect1");
+    	console.log(ip);
+    	console.log(connection);
+    	if (!connection || connection.readyState > 1){
+    		console.log("not isConnected");
+	    	connection = new WebSocket('ws://192.168.1.132:81/test', ['arduino']);
+			connection.onopen = function () {
+				console.log("connection open");
+				
+				connection.isConnected=true;
+    			console.log("readyState", connection.readyState);
+				//connection.send('hello');
+			};
+			connection.onclose = function () {
+				console.log("connection close");
+				connection.isConnected=false;
+				//connection.send('hello');
+			};
+			
+			connection.onerror = function (error) {
+				console.log('WebSocket Error ', error);
+			};
+			
+			connection.onmessage = function (e) {
+				console.log('Server: ', e.data);
+			};
+		} else{
+    	console.log(" isConnected");
+    		console.log("readyState", connection.readyState);
+			
+		}
     };
-
+	ext.disconnect = function() {
+    	console.log("disconnect");
+    	console.log(connection);
+    	connection.close();
+    };
+    ext.leftGo = function(direction) {
+    	//var json ='{"action":"leftGo","direction":"'+direction+'"}';
+    	var msg = {
+			action: "leftGo",
+			direction: direction
+		};
+    	connection.send(JSON.stringify(msg));
+    	console.log("leftGo!", msg);
+        
+    };
+    ext.leftStop = function() {
+    	var msg = {
+			action: "leftStop"
+		};
+    	console.log("leftStop!", msg);
+    	connection.send(JSON.stringify(msg));
+    };
+	ext.rightGo = function(direction) {
+    	var msg = {
+			action: "rightGo",
+			direction: direction
+		};
+    	console.log("rightGo!", msg);
+    	connection.send(JSON.stringify(msg));
+    };
+	ext.rightStop = function() {
+    	var msg = {
+			action: "rightStop"
+		};
+    	console.log("rightStop!", msg);
+    	connection.send(JSON.stringify(msg));
+    };
+	
+	ext.setSpeed = function(speed) {
+    	var msg = {
+			action: "setSpeed",
+			speed: speed
+		};
+    	console.log("setSpeed!", msg);
+    	connection.send(JSON.stringify(msg));
+    };
 
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
-            [" ", "Turn Left Wheel on %m.direction", "leftWheelOn", "forward"],
+            [" ", "Connect", "connect"],
+            [" ", "Disconnect", "disconnect"],
+            [" ", "Left Wheel Go %m.blockPos", "leftGo", "forward"],
+            [" ", "Left Wheel Stop", "leftStop"],
+            [" ", "Right Wheel Go %m.blockPos", "rightGo", "forward"],
+            [" ", "Right Wheel Stop", "rightStop"],
+            [" ", "Set Speed %m.speed", "setSpeed", 50],
+            
         ],
         menus: {
-            direction: ['forward', 'reverse']
+            blockPos: ['forward', 'reverse'],
+            speed: [10,20,30,40,50,60,70,80,90,100]
         }
     };
 
     // Register the extension
-    ScratchExtensions.register('MCPI-Scratch', descriptor, ext);
+    ScratchExtensions.register('Robot', descriptor, ext);
 
 })({});
